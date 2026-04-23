eeb965f2ac3799ed4730fed4a3b7ae275719fa7a
Ryan <96386421+RyanW848@users.noreply.github.com>
2026-04-23 14:47:58 -0400

    changed default db

--- core/db.py
+++ core/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = MongoClient(os.environ.get("MONGO_URI"))
-db = client.get_default_database()
+db = client["doyouknowball"]
 
 users_collection = db["users"]
 players_collection = db["players"]
