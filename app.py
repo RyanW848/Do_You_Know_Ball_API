80b03cde55820a43a4357a7d282a66df5083731a
Ryan <96386421+RyanW848@users.noreply.github.com>
2026-05-07 09:29:21 -0400

    remove CORS temporarily

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from services.helpers import find_player_id, convert_to_player_ids
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://www.citrus-kit.com"])
+# CORS(app, supports_credentials=True, origins=["http://localhost:3000", "https://www.citrus-kit.com"])
+CORS(app, origins="*")
 app.json.sort_keys = False
 app.register_blueprint(auth_bp)
 app.register_blueprint(api_keys_bp)
